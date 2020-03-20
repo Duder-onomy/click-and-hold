e3b0c5b125c9b5875e39aede84ccc49d463aac09
Greg Larrenaga <gregory.larrenaga@dockyard.com>
2020-03-20 10:35:00 -0700

    2.0 -> Update to es6 exports + eslint + travis ci + readme update (#1)
    * Change to es6 exports
    Install eslint, fix index to match eslint
    Add gitignore
    
    * Setup travis ci + update readme
    
    * Add note to readme
    
    * Fix travis initial builds
    
    * Update lockfile
    
    * Add travis badge to readsme

--- README.md
+++ README.md
@@ -1,23 +1,34 @@
 # Click And Hold
 
+[![Build Status](https://travis-ci.com/Duder-onomy/click-and-hold.svg?branch=master)](https://travis-ci.com/Duder-onomy/click-and-hold)
+
 Will fire a handler throttled by an interval, continuously while user presses on element.
 
 Supports touch.
 
+Zero dependencies.
+
 ### How to use:
 Important to note: If your handler relies on scope, ensure it is called with the correct scope by binding the scope directly, or using the closure.
 ```javascript
-var clickAndHold = require('click-and-hold');
+import clickAndHold from 'click-and-hold';
 
 clickAndHold.register(element, handler.bind(theScopeYouWant), interval);
 
 clickAndHold.unregister(element);
 ```
 
+### How it works:
+When the element is first interacted with (mousedown, touchstart), we start a loop that will continuously try and call a passed handler on a passed interval.
+To avoid complexity, we store the interval and subsequent handlers on the element itself. Its only 50 something lines. Scope it.
+
 ### Install:
 
 ```bash
 npm install --save-dev click-and-hold
 ```
+
+### Upgrading
+From 1.X.X -> 2.X.X, We are now using es6 module imports instead of common.js. Thinks like webpack/browserify/etc should be able to hang either way.
 
 I am not trying to camp names on NPM, so if someone else wants this name just let me know and I will gladly take my repo down.
--- index.js
+++ index.js
@@ -1,55 +1,51 @@
-'use strict';
+function handleInitialPress(element, handler, interval) {
+  element.initialPressHandler = function initialPressHandler() {
+    element.clickAndHoldInterval = setInterval(handler, interval);
+  };
 
-module.exports = {
-    register : register,
-    unregister : unregister
-};
+  return element.initialPressHandler;
+}
+
+function handlePressRelease(element) {
+  element.pressReleaseHandler = function pressReleaseHandler() {
+    clearInterval(element.clickAndHoldInterval);
+  };
+  return element.pressReleaseHandler;
+}
 
 function register(element, handler, interval) {
-    element.clickAndHoldInterval = null;
+  element.clickAndHoldInterval = null;
 
-    element.addEventListener('mousedown', _handleInitialPress(element, handler, interval), false);
+  element.addEventListener('mousedown', handleInitialPress(element, handler, interval), false);
 
-    element.addEventListener('mouseup', _handlePressRelease(element), false);
+  element.addEventListener('mouseup', handlePressRelease(element), false);
 
-    element.addEventListener('mouseleave', _handlePressRelease(element), false);
+  element.addEventListener('mouseleave', handlePressRelease(element), false);
 
-    element.addEventListener('touchstart', _handleInitialPress(element, handler, interval), false);
+  element.addEventListener('touchstart', handleInitialPress(element, handler, interval), false);
 
-    element.addEventListener('touchend', _handlePressRelease(element), false);
+  element.addEventListener('touchend', handlePressRelease(element), false);
 
-    element.addEventListener('touchcancel', _handlePressRelease(element), false);
+  element.addEventListener('touchcancel', handlePressRelease(element), false);
 }
 
 function unregister(element) {
-    element.removeEventListener('mousedown', element.initialPressHanlder, false);
+  element.removeEventListener('mousedown', element.initialPressHandler, false);
 
-    element.removeEventListener('mouseup', element.pressReleaseHandler, false);
+  element.removeEventListener('mouseup', element.pressReleaseHandler, false);
 
-    element.removeEventListener('mouseleave', element.pressReleaseHandler, false);
+  element.removeEventListener('mouseleave', element.pressReleaseHandler, false);
 
-    element.addEventListener('touchstart', element.initialPressHanlder, false);
+  element.addEventListener('touchstart', element.initialPressHandler, false);
 
-    element.addEventListener('touchend', element.pressReleaseHandler, false);
+  element.addEventListener('touchend', element.pressReleaseHandler, false);
 
-    element.addEventListener('touchcancel', element.pressReleaseHandler, false);
+  element.addEventListener('touchcancel', element.pressReleaseHandler, false);
+
+  clearInterval(element.clickAndHoldInterval);
 }
 
-// Partialy Curried.
-function _handleInitialPress(element, handler, interval) {
-    element.initialPressHanlder = function() {
-        element.clickAndHoldInterval = setInterval(handler, interval);
-    };
-
-    return element.initialPressHanlder;
-}
-
-// Partialy Curried.
-function _handlePressRelease(element) {
-    element.pressReleaseHandler = function() {
-        clearInterval(element.clickAndHoldInterval);
-    };
-    return element.pressReleaseHandler;
-}
-
-
+export default {
+  register,
+  unregister,
+};
